cdceafc795f5e50cb6884dc0a59fd490e439d540
Daniel Luke Tanoeihusada <danielluke08@gmail.com>
2020-06-23 17:32:04 -0700

    added links to navbar and add more mobile responsiveness

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 $(document).ready(()=>{
     $('.menu-toggler').on('click', () => {
+        $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
+
+    });
+
+    $('.top-nav .nav-link').on('click', () => {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
     });
 })
+
